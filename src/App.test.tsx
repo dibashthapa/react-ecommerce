aa719b75b39c7e6d49ab05bafb0bde3e2c81d7a6
Dibash Thapa <dibashthapa55@gmail.com>
2020-12-08 13:44:22 +0545

    add package.json

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render,  wait , waitForDomChange , cleanup} from "@testing-library/react";
+import images from "./data/image";
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(cleanup)  
+
+it("should match the search name",async () => {
+  const { getByText , debug } = render(<App />);
+  expect(getByText("Lime")).toBe(images[0].name)
+  await waitForDomChange()
+  debug() 
 });
